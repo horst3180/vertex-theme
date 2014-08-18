14b8b15ce4e246ed23ac5171f8ab6d3c0d27a03c
Horst3180 <horst3180@users.noreply.github.com>
2014-08-18 17:21:43 +0200

    Included everything in the main repository

--- build.sh
+++ build.sh
@@ -1,27 +1,35 @@
 #!/bin/sh
 DIR=$( cd "$( dirname "$0" )" && pwd )
-echo $DIR
 
-cp -r $DIR/Vertex-gtk $DIR/Vertex-Light
-cp -r $DIR/Vertex-gtk $DIR/Vertex
+mkdir -p $DIR/Gnome_3-12
+mkdir -p $DIR/Gnome_3-10
 
 #make light 3.12 variant
+cp -r $DIR/Vertex-gtk $DIR/Vertex-Light
 mv $DIR/Vertex-Light/gtk-2.0/gtkrc-light $DIR/Vertex-Light/gtk-2.0/gtkrc
 mv $DIR/Vertex-Light/gtk-3.0/gtk-light.css $DIR/Vertex-Light/gtk-3.0/gtk.css
 mv $DIR/Vertex-Light/index.theme-light $DIR/Vertex-Light/index.theme
 
-mkdir -p $DIR/Vertex-theme/Gnome_3-12
-cp -r $DIR/Vertex $DIR/Vertex-theme/Gnome_3-12/
-cp -r $DIR/Vertex-Light $DIR/Vertex-theme/Gnome_3-12/
+cp -r $DIR/Vertex-Light $DIR/Gnome_3-12/
+
+#make dark 3.12 variant
+cp -r $DIR/Vertex-gtk $DIR/Vertex
+rm $DIR/Vertex/index.theme-light
+#rm $DIR/Vertex/gtk-2.0/gtkrc-light
+
+cp -r $DIR/Vertex $DIR/Gnome_3-12/
 
 #make light 3.10 variant
 mv $DIR/Vertex-Light/gtk-3.0/gtk-main-3-10.css $DIR/Vertex-Light/gtk-3.0/gtk-main.css
 mv $DIR/Vertex-Light/gtk-3.0/gtk-main-dark-3-10.css $DIR/Vertex-Light/gtk-3.0/gtk-main-dark.css
 
+mv $DIR/Vertex-Light $DIR/Gnome_3-10/
+
 #make dark 3.10 variant
 mv $DIR/Vertex/gtk-3.0/gtk-main-darker-3-10.css $DIR/Vertex/gtk-3.0/gtk-main-darker.css
 mv $DIR/Vertex/gtk-3.0/gtk-main-dark-3-10.css $DIR/Vertex/gtk-3.0/gtk-main-dark.css
 
-mkdir -p $DIR/Vertex-theme/Gnome_3-10
-mv $DIR/Vertex $DIR/Vertex-theme/Gnome_3-10/
-mv $DIR/Vertex-Light $DIR/Vertex-theme/Gnome_3-10/
+mv $DIR/Vertex $DIR/Gnome_3-10/
+
+cd $DIR
+zip -r Vertex-theme Chrome Gnome_3-10 Gnome_3-12 Vertex_alt_metacity Vertex-GS LICENSE README
